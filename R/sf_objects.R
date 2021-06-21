956b382ff8784f0c494b11ec40966fc9b546883b
arrismo <mikaelmoise@protonmail.com>
2021-06-21 09:22:39 -0400

    finally figured out how to create object documentation for pennLC_sf

--- R/sf_objects.R
+++ R/sf_objects.R
@@ -1,1 +1,19 @@
-# Put roxygen2 code that documents pennLC_sf here!
+#' 
+#' @title Pennsylvania Lung Cancer SF Object
+#' @name pennLC_sf
+#' @description 
+#' pennLC_sf is a spatial object that can be used to create static or interactive maps.
+#' 
+#' @examples
+#' Static Map
+#' 
+#' ggplot() + 
+#' geom_sf(data = pennLC_sf)
+#' 
+#' Interactive Map using Leaflet
+#' 
+#' leaflet(pennLC_sf) %>%
+#' addPolygons()
+#' 
+#' @return \code{pennLC_sf}
+NULL
