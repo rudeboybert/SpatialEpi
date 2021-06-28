031b5edd6714b5d05c686d308e58e05758d1ff01
arrismo <mikaelmoise@protonmail.com>
2021-06-28 13:03:59 -0400

    updated docu for pennLC_sf

--- R/sf_objects.R
+++ R/sf_objects.R
@@ -1,15 +1,33 @@
 #' Pennsylvania Lung Cancer SF Object
 #'
-#' pennLC_sf is a spatial object that is used to create static or interactive maps.
+#' pennLC_sf is a spatial object that is used to create static or interactive maps for lunch cancer in Pennsylvania in 2002.
+#' 
+#' @format 
+#' \describe{
+#'  \item{geometry}{Counties in Pennsylvania}
+#'  \item{cases}{Number of cases}
+#'  \item{county}{Pennsylvania county}
+#'  \item{population}{Population of the county}
+#'  \item{race}{Race of the person (w = white and o = non-white) }
+#'  \item{gender}{Gender of the person (f = female and m = male)}
+#'  \item{age}{Age of the person}
+#' 
+#' }
+#' 
+#' 
+#' 
+#' 
 #' 
 #' @examples
 #' Static Map
 #' 
+#' library(ggplot2)
 #' ggplot() + 
 #' geom_sf(data = pennLC_sf)
 #' 
 #' Interactive Map using Leaflet
 #' 
+#' library(leaflet)
 #' leaflet(pennLC_sf) %>%
 #' addPolygons()
 #' 
