ca952041289ce4f41b4293e1d5d76dee2b356e2b
arrismo <mikaelmoise@protonmail.com>
2021-06-28 19:01:46 -0400

    converted NYleukemia to sf object and wrote documentation

---
+++ R/NYleukemia.R
@@ -0,0 +1,14 @@
+#' Upstate New York Leukemia SF Object
+#' 
+#' NYleukemia_sf is a spatial object that can be used to create static or interactive maps of Upstate New York Leukemia Data. 
+#' 
+#' @format A data frame with 281 rows representing airlines and 4 variables:
+#' \describe{
+#'   \item{geometry}{counties}
+#'   \item{cases}{number of cases}
+#'   \item{population}{number of people}
+#'   \item{censustract.FIPS}{unsure}
+#'   
+#' }
+#' @source Turnbull, B. W. et al (1990) Monitoring for clusters of disease: application to leukemia incidence in upstate New York \emph{American Journal of Epidemiology}, \bold{132}, 136--143
+"NYleukemia_sf"
---
+++ data-raw/NY_add_sf.R
@@ -0,0 +1,15 @@
+library(sf)
+library(plyr)
+library(dplyr)
+library(Hmisc)
+library(usethis)
+library(readr)
+library(SpatialEpi)
+
+
+NYleukemia_sf <- st_as_sf(NYleukemia$spatial.polygon)
+ny_df <- NYleukemia$data
+NYleukemia_sf <- merge(NYleukemia_sf, ny_df, by="row.names", all.x=TRUE) %>%
+  select(population,cases,censustract.FIPS,geometry)
+
+usethis::use_data(NYleukemia_sf,overwrite = TRUE)
